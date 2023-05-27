b2d4c3940612f940186f17bd0e1e130c3353cdf0
Robert James Hernandez <rob@sarcasticadmin.com>
2023-05-27 00:45:12 +0000

    this makes more sense

--- scripts/cpedict/update-cpedict.nix
+++ scripts/cpedict/update-cpedict.nix
@@ -5,24 +5,9 @@
   pkgs ? import <nixpkgs> {},
 }:
 
-pkgs.stdenv.mkDerivation rec {
-  doCheck = true;
-  pname = "update-cpedict.sh";
-  version = pkgs.lib.removeSuffix "\n" (builtins.readFile ../../VERSION);
-  src = ./update-cpedict.sh;
-
-  checkInputs = [ pkgs.shellcheck ];
-  buildInputs = [ pkgs.bash ];
-  propagatedBuildInputs = with pkgs; [ coreutils curl gnugrep gnused gzip ];
-  unpackPhase = ''
-    cp ${src} ${pname}
-  '';
-  checkPhase = ''
-    shellcheck ${pname}
-  '';
-  installPhase = ''
-    mkdir -p $out/bin
-    cp ${pname} $out/bin/${pname}
-    chmod +x $out/bin/${pname}
-  '';
+pkgs.writeShellApplication rec {
+  name = "update-cpedict.sh";
+  text = (builtins.readFile ./update-cpedict.sh);
+  runtimeInputs = with pkgs; [ coreutils curl gnugrep gnused gzip ];
+  checkPhase = true;
 }
